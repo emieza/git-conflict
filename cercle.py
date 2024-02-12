e5fd4bad9547190692b21c4e2c5979f18717caa0
Enric <emieza@xtec.cat>
2024-02-13 00:00:03 +0100

    float

--- cercle.py
+++ cercle.py
@@ -2,6 +2,6 @@
 
 radi = input("Radi de la circumferència? ")
 PI = 3.14
-perimetre = 2*PI*int(radi)
+perimetre = 2*PI*float(radi)
 print("El perímetre de la cirfumferència de radi {} és {}".format(radi,perimetre))
 
