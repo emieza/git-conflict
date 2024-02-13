619e1216078248bc4085668b8635cf134a8667e4
Enric <emieza@xtec.cat>
2024-02-13 01:04:24 +0100

    PI mes precís

--- cercle.py
+++ cercle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 radi = input("Radi de la circumferència? ")
-PI = 3.14
+PI = 3.14159
 perimetre = 2*PI*float(radi)
 print("El perímetre de la cirfumferència de radi {} és {}".format(radi,perimetre))
 
